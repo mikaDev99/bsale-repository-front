5acbfad36748977a73982ecdbd27ec06a8e0bc9a
artikuno404 <smorapuentes97@gmail.com>
2021-09-30 23:26:44 -0300

    Front End v.0.2

--- js/app.js
+++ js/app.js
@@ -1,56 +1,76 @@
 $(function(){
-   
+
+    // Loads data when page starts
     $('document').ready( function() {
         $.ajax({
-            url: 'http://localhost:4000/api/product/list',
+            url: 'http://localhost:4000/api/product/list/',
             success: function (products) {
                 let card = $('.listProducts');
-
-
                 card.html('');
                 Object.values(products.products).forEach( product => {
                     card.append(`
-                    <div class="card" style="width: 18rem;">
-                    <img src="${product.url_image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${product.name}</h5>
-                      <p class="card-text">${product.price}</p>
+                    <div class="card">
+                        <img src="${product.url_image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.name}</h5>
+                        </div>
+                        <div class="card-footer">
+                            <p class="card-text">$ ${product.price}</p>
+                            <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                        </div>
                     </div>
                     `)
                 });
+            },
+            error: function(status){
+                let card = $('.listProducts');
+                
+                card.html('');
+                card.append(`
+                <h4>No se encontraron productos :(</h4>
+                    `)
             }
         });
     });
-
-
-    $('#productForm').on('submit', function(e){
-        e.preventDefault();
-        let valueSearch = $('#valueSearch');
-        console.log(valueSearch.val());
-
-        $.ajax({
-            url: 'http://localhost:4000/api/product/search',
-            type: 'POST',
-            data: {
-                name: valueSearch.val()
-            },
-            success: function (data, response) {
-                let card = $('.listProducts');
-
-
-                card.html('');
-                Object.values(data.products).forEach( product => {
+        
+        // Send search parameters and display data
+        $('#productForm').on('submit', function(e){
+            e.preventDefault();
+            let valueSearch = $('#valueSearch');
+            console.log(valueSearch.val());
+            
+            $.ajax({
+                url: 'http://localhost:4000/api/product/search/',
+                type: 'POST',
+                data: {
+                    name: valueSearch.val()
+                },
+                success: function (data, response) {
+                    let card = $('.listProducts');
+                    card.html('');
+                    
+                    Object.values(data.products).forEach( product => {
+                        card.append(`
+                        <div class="card">
+                            <img src="${product.url_image}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.name}</h5>
+                            </div>
+                            <div class="card-footer">
+                                <p class="card-text">$ ${product.price}</p>
+                                <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                            </div>
+                        </div>
+                        `)
+                    });
+                },
+                error: function(status){
+                    let card = $('.listProducts');
+                    card.html('');
                     card.append(`
-                    <div class="card" style="width: 18rem;">
-                    <img src="${product.url_image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${product.name}</h5>
-                      <p class="card-text">${product.price}</p>
-                    </div>
-                    `)
-                });
-            }
-        })
-    })
-
+                        <h4>No se encontraron productos :(</h4>
+                            `)
+                }
+            });
+        });
 });
